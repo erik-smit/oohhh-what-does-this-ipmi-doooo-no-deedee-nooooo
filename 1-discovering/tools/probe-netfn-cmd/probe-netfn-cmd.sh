78fd3cbe72e5b07e8c17a13fc5b53f582f2b5032
erik-smit <smite@zylon.net>
2014-09-01 13:26:32 +0200

    also sweep undocumented range

--- 1-discovering/tools/probe-netfn-cmd/probe-netfn-cmd.sh
+++ 1-discovering/tools/probe-netfn-cmd/probe-netfn-cmd.sh
@@ -2,7 +2,7 @@
 
 ICMD="ipmitool -I lan -H 172.24.254.128 -U ADMIN -P ADMIN -t 0x2c raw "
 
-for i in `seq $[0x0] 2 $[0x3f]`; do
+for i in `seq $[0x0] 2 $[0xff]`; do
   for j in `seq 0 255`; do
     RAW="0x`printf %x $i` 0x`printf %x $j`"
     echo -n "$RAW |"
